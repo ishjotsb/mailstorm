74e9a1f27ba8c08bcc8e7c0efe2e5f55e2f6c9f5
Ishjot Singh Bhatia <ishjotsb26@gmail.com>
2024-12-22 15:42:59 -0800

    Integrated google oAuth

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -1,3 +1,30 @@
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+// require('dotenv').config()
+
 exports.googleAuth = async (req, res) => {
+
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+    const REDIRECT_URI = 'http://localhost:5173/home';
+    const oAuth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
     
-}
+    // console.log(CLIENT_ID, CLIENT_SECRET)
+
+  try {
+    
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+      ],
+    });
+
+    res.status(200).json({ authUrl });
+  } catch (error) {
+    console.error('Error generating auth URL:', error);
+    res.status(500).json({ error: 'Failed to generate auth URL' });
+  }
+};
---
+++ frontend/src/components/Authenticate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Authenticate() {
+  return (
+    <div>
+        <button>Sign in with Google</button>
+    </div>
+  )
+}
